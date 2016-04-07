454c363e2afcab4ba36469dbc32ff86ced9ca88f
Kent Mewhort <kent@openissues.ca>
2016-04-07 15:17:55 -0700

    Initial commit - empty project.

---
+++ lib/griddler/ses.rb
@@ -0,0 +1,7 @@
+require "griddler/ses/version"
+
+module Griddler
+  module Ses
+    # Your code goes here...
+  end
+end
---
+++ lib/griddler/ses/version.rb
@@ -0,0 +1,5 @@
+module Griddler
+  module Ses
+    VERSION = "0.1.0"
+  end
+end

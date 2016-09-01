d5bd0af29f489593288d71c9693451889024764f
Evgeniy Sukhanov <modo00@gmail.com>
2016-09-01 15:05:52 +0500

    Rails 5 support

--- lib/griddler/ses/railtie.rb
+++ lib/griddler/ses/railtie.rb
@@ -2,7 +2,14 @@ module Griddler
   module Ses
     class Railtie < Rails::Railtie
       initializer "griddler_ses.configure_rails_initialization" do |app|
-        Rails.application.middleware.insert_before ActionDispatch::ParamsParser, Griddler::Ses::Middleware
+        if ::Rails::VERSION::MAJOR >= 5
+          # Rails 5 no longer instantiates ActionDispatch::ParamsParser
+          # https://github.com/rails/rails/commit/a1ced8b52ce60d0634e65aa36cb89f015f9f543d
+          Rails.application.middleware.use Middleware
+          Rails.application.middleware.use Griddler::Ses::Middleware
+        else
+          Rails.application.middleware.insert_before ActionDispatch::ParamsParser, Griddler::Ses::Middleware
+        end
       end
     end
   end

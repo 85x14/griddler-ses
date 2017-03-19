54c22ef97ba236392c0fca8cfd6a4cc7f805159c
Chuck Callebs <chuck@callebs.io>
2017-03-18 22:36:18 -0400

    Update middleware declaration for Rails 5.

--- lib/griddler/ses/railtie.rb
+++ lib/griddler/ses/railtie.rb
@@ -1,8 +1,14 @@
 module Griddler
   module Ses
     class Railtie < Rails::Railtie
+      if Rails::VERSION::MAJOR < 5
+        middleware = ActionDispatch::ParamsParser
+      else
+        middleware = Rack::Head
+      end
+
       initializer "griddler_ses.configure_rails_initialization" do |app|
-        Rails.application.middleware.insert_before ActionDispatch::ParamsParser, Griddler::Ses::Middleware
+        Rails.application.middleware.insert_before middleware, Griddler::Ses::Middleware
       end
     end
   end
